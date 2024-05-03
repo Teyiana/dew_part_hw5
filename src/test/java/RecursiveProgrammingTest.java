e1bd1c7b03f13ecfef5b7f7a773e737342dcf3ed
Tatiana Zayarnyuk <tytyshkamalyshka@gmail.com>
2024-05-03 13:45:03 +0300

    test

--- src/test/java/RecursiveProgrammingTest.java
+++ src/test/java/RecursiveProgrammingTest.java
@@ -1,13 +1,19 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class RecursiveProgrammingTest {
     private RecursiveProgramming recursive = new RecursiveProgramming();
 
+    private final long[] expected = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
+
     @Test
     public void getFibonacciValueTest() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(recursive.getFibonacciValue(i));
+        for (int i = 0; i < expected.length; i++) {
+            long result = recursive.getFibonacciValue(i);
+            assertEquals(expected[i], result);
+            System.out.println(result);
         }
     }
 
